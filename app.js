21e87f8c45f74819ec0329977dcb5a25b8e1da79
dpinchuk <pinchuk.dap@gmail.com>
2019-09-06 13:11:23 +0300

    refactoring

--- app.js
+++ app.js
@@ -109,31 +109,12 @@ app.use((req, res, next) => {
 //     });
 // });
 app.use((req, res, next) => {
-  res.status(404).json({ err: "404" });
+  res.status(500).json({ err: "500" });
 });
 
 app.use((err, req, res, next) => {
   console.log(err.stack);
-  res.status(500).json({ err: "500" });
+  res.status(404).json({ err: "404" });
 });
-
-let checkForUserAuthorization = (
-  res,
-  id,
-  email,
-  notAuthUserPage,
-  authUserPage
-) => {
-  if (!id && !email) {
-    res.render(notAuthUserPage);
-  } else {
-    res.render(authUserPage, {
-      user: {
-        id: id,
-        email: email,
-      },
-    });
-  }
-};
 
 module.exports = app;
