4464f114ebfa2ae8409f3062a0cea8b330123701
dpinchuk <pinchuk.dap@gmail.com>
2019-09-12 22:00:18 +0300

    added courses to admin account

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
+const Courses = require("../models/courses");
+
+router.get("/", (req, res) => {
+  User.find({}).then((users, courses) => {
+    res.render("../views/admin/admin", {
+      users: users,
+      courses: courses
+    });
+  });
+});
 
 router.get("/users", (req, res) => {
   User.find({}).then(users => {
     res.render("../views/admin/users", { users: users });
+  });
+});
+
+router.get("/courses", (req, res) => {
+  Courses.find({}).then(courses => {
+    res.render("../views/admin/courses", { courses: courses });
   });
 });
 
