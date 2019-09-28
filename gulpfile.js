0063178dcd1e8c9a6d9877953a018dd2bb93a6dc
Irina <irinastoetski13@gmail.com>
2019-09-28 11:02:17 +0300

    Started migration on less

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 const gulp = require("gulp");
 const path = require('path');
 const less = require("gulp-less");
+
 /*
-const autoprefixer = require("gulp-autoprefixer");
 const cssnano = require("gulp-cssnano");
 const plumber = require("gulp-plumber"); */
 
