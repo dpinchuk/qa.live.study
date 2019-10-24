ec7012fb809ef711a89f5df7cc0fa87a55f32b3e
dpinchuk <pinchuk.dap@gmail.com>
2019-10-24 16:41:10 +0300

    added courses pages and handlers

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,9 @@
 const sign = require("./sign");
 const user = require("./user");
 const admin = require("./admin");
-const courses = require("./courses")
 
 module.exports = {
   sign,
   user,
   admin,
-  courses,
 };
