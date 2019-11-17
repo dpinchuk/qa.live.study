5c7a12ae30d3e8f182e8e5462d0e1a63da404e43
Alex <alexneo2003@gmail.com>
2019-11-17 20:59:10 +0200

    fix registration page

--- public/js/register.js
+++ public/js/register.js
@@ -1,7 +1,7 @@
 $(() => {
   let flag = true;
   // register
-  $(".register-button").on("click", e => {
+  $("#regForm").on("submit", e => {
     e.preventDefault();
     let data = {
       name: $("#register-name").val(),
@@ -17,6 +17,7 @@ $(() => {
       contentType: "application/json",
       url: "/reg",
     }).done(data => {
+    console.log(data);
       if (data.success) {
         regResult("User created successfully!", "alert-success");
         $(location).attr("href", "/user/account");
@@ -29,21 +30,19 @@ $(() => {
 });
 
 let regResult = (text, clazz) => {
-  $(".reg-form").after(
-    '<div id="errorAlert" class="alert ' +
-      clazz +
-      ' alert-dismissible fade show position-absolute text-center" role="alert" style="top: 0%;right: 0%;left: 0%">' +
-      '   <button type="button" class="close" data-dismiss="alert" aria-label="Close">' +
-      '       <span aria-hidden="true">×</span>' +
-      "   </button>" +
-      text +
-      "</div>" +
-      "<script>" +
-      "   $(() => {" +
-      "       window.setTimeout(() => {" +
-      "           $('#errorAlert').alert('close');" +
-      "       }, 2000);" +
-      "   });" +
-      "</script>"
+  $("#regForm").after(
+    `<div id="errorAlert" class="alert ${clazz} alert-dismissible fade show position-absolute text-center" role="alert" style="top: 0%;right: 0%;left: 0%">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">×</span>
+          </button>
+      ${text}
+      </div>
+      <script>
+         $(() => {
+             window.setTimeout(() => {
+                 $('#errorAlert').alert('close');
+             }, 2000);
+         });
+      </script>`
   );
 };
